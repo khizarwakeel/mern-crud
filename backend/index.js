706609ebf717ca231e26e945d9fd6ba1b7750ea8
Khizar Wakeel <khizarwakeel@gmail.com>
2024-06-25 21:15:22 +0500

    added envs and mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    res.send("Api is running! Wow")
-})
+  res.send("Api is running! Wow");
+});
 
-app.listen(4000)
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("DB Connected Successfully!");
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error("Error starting server:", error);
+  } else {
+    console.log(`Server is listening on port ${port}`);
+  }
+});
